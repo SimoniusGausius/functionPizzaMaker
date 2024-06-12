1f2587b59da67ac74b8c2bc74a021ffd476cd523
Simon Gaus <simon.gaus@ict-campsu.net>
2024-06-12 08:10:16 +0200

    delete create function

---
+++ src/functions/httpTriggerPizzaMakerDELETE.js
@@ -0,0 +1,28 @@
+//funktioniert nicht
+
+const { app,output } = require('@azure/functions');
+
+const cosmosOutput = output.cosmosDB({
+    databaseName: 'PizzaMaker',
+    containerName: 'PizzaCreations',
+    connection: 'CosmosDB',
+    createIfNotExists: true
+});
+
+app.http('httpTriggerPizzaMakerPOST', {
+    methods: ['POST'],
+    authLevel: 'anonymous',
+    extraInputs: [cosmosOutput],
+    route: 'items',
+    handler: async (request, context) => {
+
+        const pizzaPost = await request.json();
+
+        context.extraOutputs.set(cosmosOutput, pizzaPost)
+
+        return {
+            body: JSON.stringify(pizzaPost),
+            status: 201
+        }
+    }
+});
