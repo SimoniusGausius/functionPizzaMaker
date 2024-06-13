e50e2db84149ab021f0de4553b3da41be50ea4bd
Simon Gaus <simon.gaus@ict-campsu.net>
2024-06-13 09:15:33 +0200

    key, endpoint secure for push

--- src/functions/httpTriggerPizzaMakerDELETE.js
+++ src/functions/httpTriggerPizzaMakerDELETE.js
@@ -1,8 +1,8 @@
 const { app } = require("@azure/functions");
 const { CosmosClient } = require("@azure/cosmos");
 
-const endpoint = "https://pizzamaker.documents.azure.com:443/";
-const key = "vVaKKGYBYesw5L3yZrqw2Zr8ZRfzeVoH9MGVnkOGaXGGI8Qd5f5sIzyHeigW28QQA6ax2b6wuvTVACDbmzXEAg==";
+const endpoint = process.env.DBENDPOINT
+const key = process.env.DBKEY
 
 const client = new CosmosClient({ endpoint, key });
 
@@ -18,9 +18,7 @@ app.http("httpTriggerPizzaMakerDELETE", {
       const container = database.container(containerId);
   
       const itemId = request.params.id;
-  
-      //const { resource: deletedItem } = await container.item(itemId).delete();
-      // Falls die obere Zeile nicht funktioniert, bitte die untenstehende kommentierte Zeile brauchen
+      
       const { resource: deletedItem } = await container.item(itemId, itemId).delete();
       return {
         body: JSON.stringify(deletedItem),
